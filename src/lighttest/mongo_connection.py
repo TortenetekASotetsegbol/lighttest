a63d0b31c17969604e2ad6f9978bdacd9ad20cdc
Tóth Dávid <toth.david@matrix2.meditcom.hu>
2022-12-04 12:14:34 +0100

    the mongodb connection extended with a new dialecxt driver component. Now can be change the dialect_driver and the client_url separetely

--- src/lighttest/mongo_connection.py
+++ src/lighttest/mongo_connection.py
@@ -6,19 +6,29 @@ from pymongo import mongo_client as mc
 
 
 class Mongo:
-    default_mongo_client: str = "mongodb://localhost:27017"
+    dialect_driver: str = "mongodb"
+    client_url: str = "localhost:27017"
+    default_mongo_client: str = f'mongodb://localhost:27017'
     default_db: str = "default"
-    current_client = default_mongo_client
+    current_client: str = default_mongo_client
     current_database = default_db
 
     client = mc.MongoClient(default_mongo_client)
+
     database = client[default_db]
     collection = database["teszt"]
 
     @staticmethod
-    def set_client(client_url: str) -> None:
-        Mongo.client = mc.MongoClient(f'mongodb://{client_url}')
-        Mongo.current_client = f'mongodb://{client_url}'
+    def set_client(new_client_url: str) -> None:
+        Mongo.client_url = new_client_url
+        Mongo.client = mc.MongoClient(f'{Mongo.dialect_driver}://{Mongo.client_url}')
+        Mongo.current_client = f'{Mongo.dialect_driver}://{Mongo.client_url}'
+
+    @staticmethod
+    def set_dialect_driver(new_dialect_driver: str) -> None:
+        Mongo.dialect_driver = new_dialect_driver
+        Mongo.set_client(Mongo.client_url)
+        Mongo.current_client = f'{Mongo.dialect_driver}://{Mongo.client_url}'
 
     @staticmethod
     def set_database(database_name: str):
